82a93beccd3085e40655e1fd103de0f5ca556fce
Kanae Tani <kanaetani92@gmail.com>
2025-10-26 17:39:02 +0900

    Fix assessment store callbacks to avoid rerenders

--- src/components/AssessmentStore.tsx
+++ src/components/AssessmentStore.tsx
@@ -2,6 +2,7 @@
 
 import {
   createContext,
+  useCallback,
   useContext,
   useEffect,
   useMemo,
@@ -69,26 +70,35 @@ export function AssessmentProvider({ children }: { children: React.ReactNode }) 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
   }, [data, hasHydrated]);
 
-  const api = useMemo<AssessmentContext>(() => ({
-    data,
-    setLikert: (path, index, val) => {
-      setData((prev) => {
-        const next = cloneAssessmentData(prev);
-        const arr = path.split('.').reduce<any>((acc, key) => acc[key], next) as Likert5[];
-        arr[index] = val;
-        return next;
-      });
-    },
-    setStage: (stage) => {
-      setData((prev) => ({ ...prev, stage }));
-    },
-    reset: () => {
-      skipNextPersist.current = true;
-      localStorage.removeItem(STORAGE_KEY);
-      setData(createDefaultAssessment());
-    },
-    hasHydrated,
-  }), [data, hasHydrated]);
+  const setLikert = useCallback((path: string, index: number, val: Likert5) => {
+    setData((prev) => {
+      const next = cloneAssessmentData(prev);
+      const arr = path.split('.').reduce<any>((acc, key) => acc[key], next) as Likert5[];
+      arr[index] = val;
+      return next;
+    });
+  }, []);
+
+  const setStage = useCallback((stage: Stage) => {
+    setData((prev) => ({ ...prev, stage }));
+  }, []);
+
+  const reset = useCallback(() => {
+    skipNextPersist.current = true;
+    localStorage.removeItem(STORAGE_KEY);
+    setData(createDefaultAssessment());
+  }, []);
+
+  const api = useMemo<AssessmentContext>(
+    () => ({
+      data,
+      setLikert,
+      setStage,
+      reset,
+      hasHydrated,
+    }),
+    [data, hasHydrated, reset, setLikert, setStage],
+  );
 
   return <AssessmentCtx.Provider value={api}>{children}</AssessmentCtx.Provider>;
 }
